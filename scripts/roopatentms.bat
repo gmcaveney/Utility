9c246196c805a6c6cdb50305ce4dd07129a5967b
gmcaveney <gmcaveney@selectivetechnology.com>
2011-12-30 15:54:18 -0500

    Modified script paths, added table for raw html

--- scripts/roopatentms.bat
+++ scripts/roopatentms.bat
@@ -1,7 +1,7 @@
 set OLDDIR=%CD%
-cd c:\code
+cd c:\gitrepo\utility
 rmdir /S /Q patentms
 mkdir patentms
 cd patentms
-roo script --file c:\code\scripts\patentms.roo > %OLDDIR%\patentms.log
+roo script --file c:\gitrepo\utility\scripts\patentms.roo > %OLDDIR%\patentms.log
 cd %OLDDIR%
